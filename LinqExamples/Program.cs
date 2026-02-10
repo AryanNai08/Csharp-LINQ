e081201fc244dd539949f3058aeb85a386fa488c
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-10 17:32:51 +0530

    Added .gitattribute & .gitignore

---
+++ LinqExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+namespace LinqExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            int[] age = { 12, 30, 32, 34, 53, 11, 2, 3, 4, 66 };
+
+            //Select * from age where age > 18 in sql
+
+            var a=from i in age where i > 18 select i;
+
+
+            foreach(var i in a)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("****************");
+            Console.WriteLine("ascending order");
+
+            //var b=from i in age where i>20 orderby i descending select i;  //desc order
+            var b = from i in age where i > 20 orderby i  select i;
+
+            foreach (var i in a)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("**************");
+        }
+    }
+}
